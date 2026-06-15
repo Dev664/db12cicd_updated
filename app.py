522828ca98bc8327b958c6668e1429c305cf0b91
Debdip Ghosh <debdip201@gmail.com>
2026-06-15 20:16:26 +0530

    updated

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ HTML = """
 
 <div class="card">
     <h1>🚀 CI/CD Demo project: Application Deployment </h1>
-    <h2>Version 1</h2>
+    <h2>Version 2</h2>
 
     <p>Successfully deployed by Debdip Ghosh:</p>
 
